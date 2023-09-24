0729e402271fe098939ccab1b674ab9765a12039
Sinomor <navifull7@gmail.com>
2023-09-24 12:24:27 +0300

    update README

--- home/.config/awesome/ui/notifs.lua
+++ home/.config/awesome/ui/notifs.lua
@@ -57,7 +57,7 @@ naughty.connect_signal("request::display", function(n)
 naughty.layout.box {
 	notification = n,
 	maximum_width = 900,
-	maximum_height = 520,
+	maximum_height = 120,
 	widget_template = {
 		widget = wibox.container.constraint,
 		strategy = "max",
